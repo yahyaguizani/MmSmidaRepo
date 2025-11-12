2938506bf3287cbf50ac30f39f4274bc1ba64968
yahya guizani <yahyaguizani1919@gmail.com>
2025-11-13 00:07:02 +0100

    version de base

---
+++ auth-microservice/frontend/src/SignIn.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://backend.local/signin", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({username, password})
+    });
+    if(res.status === 200) {
+      alert("Connexion réussie !");
+    } else {
+      alert("Erreur login/password");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Username" onChange={e=>setUsername(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={e=>setPassword(e.target.value)} />
+      <button type="submit">Sign In</button>
+    </form>
+  );
+}
